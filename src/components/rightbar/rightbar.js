b470f940994d3d0d967e9e62224e71e6028f1d30
coolicedan1 <coolicedan@gmail.com>
2023-07-14 18:35:37 +0100

    create the rightbar

--- src/components/rightbar/rightbar.js
+++ src/components/rightbar/rightbar.js
@@ -3,7 +3,86 @@ import "./rightbar.css";
 const rightbar = () => {
   return(
     <div className="rightbar">
-      rightbar
+      <div className="rightbarwrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="/Assets/gift.png" alt="birthday image" ></img>
+          <span className="birthdayText"><b>Elias Ifeoma</b> and <b> 5 other friends</b> have a birthday today.</span>
+        </div>
+        <img className="rightbarAd" src="/Assets/ads.jpg" alt=""></img>
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className="rightbarProfileImg" src="/Assets/person/4.jpg.jpg" alt=""></img>
+              <div className="rightbarOnline"></div>
+            </div>
+            <span className="rightbarUsername">Daniel Isaac</span>
+          </li>
+        </ul>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className="rightbarProfileImg" src="/Assets/person/4.jpg.jpg" alt=""></img>
+              <div className="rightbarOnline"></div>
+            </div>
+            <span className="rightbarUsername">Daniel Isaac</span>
+          </li>
+        </ul>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className="rightbarProfileImg" src="/Assets/person/4.jpg.jpg" alt=""></img>
+              <div className="rightbarOnline"></div>
+            </div>
+            <span className="rightbarUsername">Daniel Isaac</span>
+          </li>
+        </ul>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className="rightbarProfileImg" src="/Assets/person/4.jpg.jpg" alt=""></img>
+              <div className="rightbarOnline"></div>
+            </div>
+            <span className="rightbarUsername">Daniel Isaac</span>
+          </li>
+        </ul>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className="rightbarProfileImg" src="/Assets/person/4.jpg.jpg" alt=""></img>
+              <div className="rightbarOnline"></div>
+            </div>
+            <span className="rightbarUsername">Daniel Isaac</span>
+          </li>
+        </ul>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className="rightbarProfileImg" src="/Assets/person/4.jpg.jpg" alt=""></img>
+              <div className="rightbarOnline"></div>
+            </div>
+            <span className="rightbarUsername">Daniel Isaac</span>
+          </li>
+        </ul>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className="rightbarProfileImg" src="/Assets/person/4.jpg.jpg" alt=""></img>
+              <div className="rightbarOnline"></div>
+            </div>
+            <span className="rightbarUsername">Daniel Isaac</span>
+          </li>
+        </ul>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img className="rightbarProfileImg" src="/Assets/person/4.jpg.jpg" alt=""></img>
+              <div className="rightbarOnline"></div>
+            </div>
+            <span className="rightbarUsername">Daniel Isaac</span>
+          </li>
+        </ul>
+      </div>
     </div>
 
   )
